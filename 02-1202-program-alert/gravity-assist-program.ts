ecc32f78c788aeca6871cb7fdcf611c6e0a41f2f
Eugene Kwok <ekwok@relias.com>
2020-02-04 22:40:06 -0500

    1202 Program Alarm: Part 1

--- 02-1202-program-alert/gravity-assist-program.ts
+++ 02-1202-program-alert/gravity-assist-program.ts
@@ -1,1 +1,42 @@
-export class GravityAssistProgram {}
+export class GravityAssistProgram {
+  get intcodeProgram() {
+    return this._intcodeProgram;
+  }
+
+  private _intcodeProgram: number[];
+
+  constructor(...intcodeProgram: number[]) {
+    this._intcodeProgram = intcodeProgram;
+  }
+
+  intcodeSwap(num: number, index: number): void {
+    this._intcodeProgram[index] = num;
+  }
+
+  run(): number[] {
+    for (let i = 0; i < this._intcodeProgram.length; i += 4) {
+      this.process(i);
+    }
+
+    return this._intcodeProgram;
+  }
+
+  private process(index: number): void {
+    switch (this._intcodeProgram[index]) {
+      case 1: {
+        this._intcodeProgram[this._intcodeProgram[index + 3]] =
+          this._intcodeProgram[this._intcodeProgram[index + 1]] +
+          this._intcodeProgram[this._intcodeProgram[index + 2]];
+        break;
+      }
+      case 2: {
+        this._intcodeProgram[this._intcodeProgram[index + 3]] =
+          this._intcodeProgram[this._intcodeProgram[index + 1]] *
+          this._intcodeProgram[this._intcodeProgram[index + 2]];
+      }
+      case 99: {
+        return;
+      }
+    }
+  }
+}
