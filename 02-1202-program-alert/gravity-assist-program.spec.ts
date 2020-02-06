04bd87ee4c673165dda385a7ed83c35807fac2fb
Eugene Kwok <ekwok@relias.com>
2020-02-05 19:13:07 -0500

    1202 Program - unit test update

--- 02-1202-program-alert/gravity-assist-program.spec.ts
+++ 02-1202-program-alert/gravity-assist-program.spec.ts
@@ -5,21 +5,21 @@ describe('Gravity Assist Program', () => {
 
   it('should process sample 1', () => {
     gaProgram = new GravityAssistProgram(1, 0, 0, 0, 99);
-    expect(gaProgram.run()[0]).toBe(2);
+    expect(gaProgram.getOutput(0)).toBe(2);
   });
 
   it('should process sample 2', () => {
     gaProgram = new GravityAssistProgram(2, 3, 0, 3, 99);
-    expect(gaProgram.run()[3]).toBe(6);
+    expect(gaProgram.getOutput(3)).toBe(6);
   });
 
   it('should process sample 3', () => {
     gaProgram = new GravityAssistProgram(2, 4, 4, 5, 99, 0);
-    expect(gaProgram.run()[5]).toBe(9801);
+    expect(gaProgram.getOutput(5)).toBe(9801);
   });
 
   it('should process sample 4', () => {
     gaProgram = new GravityAssistProgram(1, 1, 1, 4, 99, 5, 6, 0, 99);
-    expect(gaProgram.run()[0]).toBe(30);
+    expect(gaProgram.getOutput(0)).toBe(30);
   });
 });
