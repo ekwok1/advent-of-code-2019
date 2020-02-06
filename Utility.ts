8d03f755e9a82b21b7b299fc19e2c27471a8fb67
Eugene Kwok <ekwok@relias.com>
2020-02-05 22:36:48 -0500

    1202 Program Alert - clean up Utility

--- Utility.ts
+++ Utility.ts
@@ -1,16 +1,11 @@
 import fs = require('fs');
 
 export class Utility {
-  static getArgsFromPath(path: string, delimiter: string): number[] {
-    var contents = this.readFile(path);
-    return contents.split(delimiter).map(arg => Number(arg));
-  }
-
   static getArgsFromString(str: string, delimiter: string = ','): number[] {
     return str.split(delimiter).map(arg => Number(arg));
   }
 
-  private static readFile(path: string): string {
+  static readFile(path: string): string {
     return fs.readFileSync(path, 'utf-8');
   }
 }
