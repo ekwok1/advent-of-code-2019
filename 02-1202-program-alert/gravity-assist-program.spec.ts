5553182b98a73f45672e7082f3810e868fa9bf03
Eugene Kwok <ekwok@relias.com>
2020-02-05 19:24:50 -0500

    1202 Program - Part 2 - unit test

--- 02-1202-program-alert/gravity-assist-program.spec.ts
+++ 02-1202-program-alert/gravity-assist-program.spec.ts
@@ -22,4 +22,13 @@ describe('Gravity Assist Program', () => {
     gaProgram = new GravityAssistProgram(1, 1, 1, 4, 99, 5, 6, 0, 99);
     expect(gaProgram.getOutput(0)).toBe(30);
   });
+
+  it('should find noun and verb', () => {
+    gaProgram = new GravityAssistProgram(1, 0, 0, 0, 99, 1, 49);
+    const obj = {
+      noun: 5,
+      verb: 6
+    };
+    expect(gaProgram.findNounAndVerb(50)).toBe(obj);
+  });
 });
