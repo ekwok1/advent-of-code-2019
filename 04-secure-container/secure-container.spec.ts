83a25c8801c7b94000768a8111f604f119b4592f
Eugene Kwok <ekwok@relias.com>
2020-02-08 18:54:07 -0500

    Secure Container - Part 1 - unit test update

--- 04-secure-container/secure-container.spec.ts
+++ 04-secure-container/secure-container.spec.ts
@@ -1,25 +1,31 @@
 import { SecureContainer } from './secure-container';
 
-describe('Crossed Wires Program', () => {
+describe('Secure Container', () => {
   let container: SecureContainer;
 
-  beforeAll(() => {
-    container = new SecureContainer(100000, 999998);
+  it('should throw an error for invalid lower bound', () => {
+    expect(() => new SecureContainer(1, 999999)).toThrowError('Lower bound of 1 is invalid');
   });
 
-  it('should be six digit number', () => {
-    expect(container.check(99999)).toBeFalse();
+  it('should throw an error for invalid upper bound', () => {
+    expect(() => new SecureContainer(100000, 1000000)).toThrowError('Upper bound of 1000000 is invalid');
   });
 
-  it(`should be within container's range`, () => {
-    expect(container.check(999999)).toBeFalse();
-  });
+  describe('With a valid secure container', () => {
+    beforeAll(() => {
+      container = new SecureContainer(100000, 999998);
+    });
 
-  it('should have at least two adjacent digits that are the same', () => {
-    expect(container.check(123789)).toBeFalse();
-  });
+    it(`should be within container's range`, () => {
+      expect(container.check(999999)).toBeFalse();
+    });
 
-  it('should never have digits that decrease from left to right', () => {
-    expect(container.check(223450)).toBeFalse();
+    it('should have at least two adjacent digits that are the same', () => {
+      expect(container.check(123789)).toBeFalse();
+    });
+
+    it('should never have digits that decrease from left to right', () => {
+      expect(container.check(223450)).toBeFalse();
+    });
   });
 });
