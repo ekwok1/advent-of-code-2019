32ad5d29bcc54997f0795dd705a408b65d68c19e
Eugene Kwok <ekwok@relias.com>
2020-02-08 17:56:51 -0500

    Crossed Wires - Part 2

--- 03-crossed-wires/crossed-wires.ts
+++ 03-crossed-wires/crossed-wires.ts
@@ -1,21 +1,20 @@
-import { WireCoordinates } from './wire-coordinates';
+import { WireCoordinates } from "./wire-coordinates";
 
 export class CrossedWires {
-  private _wire1Path: Set<string>;
-  private _wire2Path: Set<string>;
+  private _wire1Map = new Map<string, number>();
+  private _wire2Map = new Map<string, number>();
+  private _intersections: Set<string>;
 
   constructor(wire1: string, wire2: string) {
-    this._wire1Path = this.writeWirePath(wire1);
-    this._wire2Path = this.writeWirePath(wire2);
+    this.writeWirePath(wire1, this._wire1Map);
+    this.writeWirePath(wire2, this._wire2Map);
+    this._intersections = new Set<string>([...this._wire1Map.keys()].filter(x => this._wire2Map.get(x)));
   }
 
   findClosestIntersection(): number {
-    const intersections = new Set<string>(
-      [...this._wire1Path].filter(x => this._wire2Path.has(x))
-    );
     let minDistance = Number.MAX_SAFE_INTEGER;
 
-    intersections.forEach(intersection => {
+    this._intersections.forEach(intersection => {
       const coords = JSON.parse(intersection) as WireCoordinates;
       const manhattanDistance = Math.abs(coords.x) + Math.abs(coords.y);
       minDistance = Math.min(manhattanDistance, minDistance);
@@ -24,54 +23,64 @@ export class CrossedWires {
     return minDistance;
   }
 
-  private writeWirePath(wire: string): Set<string> {
+  findShortestIntersection(): number {
+    let minSteps = Number.MAX_SAFE_INTEGER;
+
+    this._intersections.forEach(intersection => {
+      const wire1Steps = this._wire1Map.get(intersection);
+      const wire2Steps = this._wire2Map.get(intersection);
+      minSteps = Math.min(wire1Steps + wire2Steps, minSteps);
+    });
+
+    return minSteps;
+  }
+
+  private writeWirePath(wire: string, map: Map<string, number>): void {
     let currentX = 0;
     let currentY = 0;
-    let current: WireCoordinates;
-    const wirePath = wire.split(',');
-    const path = new Set<string>();
+    let currentSteps = 0;
+    const wirePath = wire.split(",");
 
     wirePath.forEach(instruction => {
       let direction = instruction[0];
       let steps = Number(instruction.slice(1));
       switch (direction) {
-        case 'R': {
-          while (steps) {
-            current = { x: ++currentX, y: currentY } as WireCoordinates;
-            this.addCoordinates(path, current);
-            steps--;
+        case "R": {
+          for (let i = 0; i < steps; i++) {
+            this.addCoordinates(map, ++currentX, currentY, ++currentSteps);
           }
+          break;
         }
-        case 'L': {
-          while (steps) {
-            current = { x: --currentX, y: currentY } as WireCoordinates;
-            this.addCoordinates(path, current);
-            steps--;
+        case "L": {
+          for (let i = 0; i < steps; i++) {
+            this.addCoordinates(map, --currentX, currentY, ++currentSteps);
           }
+          break;
         }
-        case 'U': {
-          while (steps) {
-            current = { x: currentX, y: ++currentY } as WireCoordinates;
-            this.addCoordinates(path, current);
-            steps--;
+        case "U": {
+          for (let i = 0; i < steps; i++) {
+            this.addCoordinates(map, currentX, ++currentY, ++currentSteps);
           }
+          break;
         }
-        case 'D': {
-          while (steps) {
-            current = { x: currentX, y: --currentY } as WireCoordinates;
-            this.addCoordinates(path, current);
-            steps--;
+        case "D": {
+          for (let i = 0; i < steps; i++) {
+            this.addCoordinates(map, currentX, --currentY, ++currentSteps);
           }
+          break;
         }
       }
     });
-
-    return path;
   }
 
-  private addCoordinates(path: Set<string>, current: WireCoordinates): void {
-    if (!path.has(JSON.stringify(current))) {
-      path.add(JSON.stringify(current));
+  private addCoordinates(map: Map<string, number>, currentX: number, currentY: number, steps: number): void {
+    const current = {
+      x: currentX,
+      y: currentY
+    } as WireCoordinates;
+    const curr = JSON.stringify(current);
+    if (!map.has(curr)) {
+      map.set(curr, steps);
     }
   }
 }
