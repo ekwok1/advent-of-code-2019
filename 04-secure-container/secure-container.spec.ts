1923ea8700da610a18ca450e122cd06ee0141fd7
Eugene Kwok <ekwok@relias.com>
2020-02-08 22:03:04 -0500

    Secure Container - Part Two - update unit tests

--- 04-secure-container/secure-container.spec.ts
+++ 04-secure-container/secure-container.spec.ts
@@ -11,7 +11,7 @@ describe('Secure Container', () => {
     expect(() => new SecureContainer(100000, 1000000)).toThrowError('Upper bound of 1000000 is invalid');
   });
 
-  describe('With a valid secure container', () => {
+  describe('with a valid lower and upper bound', () => {
     beforeAll(() => {
       container = new SecureContainer(100000, 999998);
     });
@@ -22,6 +22,23 @@ describe('Secure Container', () => {
 
     it('should have at least two adjacent digits that are the same', () => {
       expect(container.check(123789)).toBeFalse();
+    });
+
+    it('should have at least two adjacent digits that are not part of a larger group of matching digits', () => {
+      // all
+      expect(container.check(111111)).toBeFalse();
+      // 5:1, 1:5
+      expect(container.check(111112)).toBeFalse();
+      expect(container.check(122222)).toBeFalse();
+      // 4:1:1, 1:4:1, 1:1:4
+      expect(container.check(111123)).toBeFalse();
+      expect(container.check(122223)).toBeFalse();
+      expect(container.check(123333)).toBeFalse();
+      // 3:1:1:1, 1:3:1:1, 1:1:3:1, 1:1:1:3
+      expect(container.check(111234)).toBeFalse();
+      expect(container.check(122234)).toBeFalse();
+      expect(container.check(123334)).toBeFalse();
+      expect(container.check(123444)).toBeFalse();
     });
 
     it('should never have digits that decrease from left to right', () => {
