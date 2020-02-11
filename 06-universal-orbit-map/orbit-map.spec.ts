fdb964e011ef0a2571a185a1088e6372e66feb2b
Eugene Kwok <ekwok@relias.com>
2020-02-10 19:15:21 -0500

    Universal Orbit Map - Part 1 - unit test

---
+++ 06-universal-orbit-map/orbit-map.spec.ts
@@ -0,0 +1,21 @@
+import { OrbitMap } from './orbit-map';
+
+describe('Orbit Map', () => {
+  let orbitMap: OrbitMap;
+  const map = `COM)B
+  B)C
+  C)D
+  D)E
+  E)F
+  B)G
+  G)H
+  D)I
+  E)J
+  J)K
+  K)L`;
+
+  it('should return number of direct and indirect orbits', () => {
+    orbitMap = new OrbitMap(map);
+    expect(orbitMap.getTotalOrbits()).toBe(42);
+  });
+});
