64a0f6c044880a96d0b006371340e6e0ef7dbf19
Eugene Kwok <ekwok@relias.com>
2020-02-13 18:08:06 -0500

    Universal Orbit Map - Part 2 - unit test update

--- 06-universal-orbit-map/orbit-map.spec.ts
+++ 06-universal-orbit-map/orbit-map.spec.ts
@@ -4,6 +4,7 @@ fdescribe('Orbit Map', () => {
   let orbitMap: OrbitMap;
   const map = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L'];
   const santaMap = [...map.slice(), 'K)YOU', 'I)SAN'] as string[];
+  const altSantaMap = [...map.slice(), 'K)YOU', 'F)SAN'] as string[];
 
   it('should return number of direct and indirect orbits no matter the order', () => {
     orbitMap = new OrbitMap(shuffle(map));
@@ -13,6 +14,11 @@ fdescribe('Orbit Map', () => {
   it('should return the number of orbital transfers to get from point A to point B', () => {
     orbitMap = new OrbitMap(shuffle(santaMap));
     expect(orbitMap.getOrbitalTransfers('YOU', 'SAN')).toBe(4);
+  });
+
+  it('should return the number of orbital transfers to get from point A to point B even if not direct path', () => {
+    orbitMap = new OrbitMap(shuffle(altSantaMap));
+    expect(orbitMap.getOrbitalTransfers('YOU', 'SAN')).toBe(3);
   });
 
   function shuffle(array: string[]): string[] {
